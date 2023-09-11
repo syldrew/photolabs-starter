d2e197200a43a93b2206a9c5c126ae674a35cb3a
syldrew <syl_peke14@hotmail.com>
2023-09-11 11:52:50 -0500

    Add the PhotoListItem component, and Display photo on the homepage

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,26 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//   username: "Joe Example",
+//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+// };
 
-const PhotoListItem = () => {
-  /* Insert React */
-};
+const PhotoListItem = (props) => {
+    return (
+      <div>
+        <img src={props.data.imageSource}></img>
+        <img src={props.data.profile}></img>
+        <p>{props.data.username}</p>
+        <p>{props.data.location.city}, {props.data.location.country}</p>
+      </div>
+    );
+  };
 
 export default PhotoListItem;
