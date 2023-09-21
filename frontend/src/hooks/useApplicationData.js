95b32a2376d99037a8704a6a7f58f403f392e3d4
syldrew <syl_peke14@hotmail.com>
2023-09-21 11:13:12 -0500

    Implement UseReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,24 +1,82 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+// export const useApplicationData = () => {
+//   const [favs, setFavs] = useState([]);
+//   const [selectedPhoto, setSelectedPhoto] = useState(null);
+//   const [modal, setModal] = useState(false);
+//   const updateToFavPhotoIds = (photoId) => {
+//     favs.includes(photoId) ? setFavs(favs.filter(e => e !== photoId)) : setFavs([...favs, photoId]);
+//   };
+//     const setPhotoSelected = (photo) => {
+//     setModal(true);
+//     setSelectedPhoto(photo);
+//   };
+// const onClosePhotoDetailsModal = () => {
+//     setModal(false);
+//     setSelectedPhoto(null);
+//   };
+//   return {
+//     state: { favs, selectedPhoto, modal },
+//     updateToFavPhotoIds,
+//     setPhotoSelected,
+//     onClosePhotoDetailsModal
+
+//   };
+// };
+
+import { useReducer } from 'react';
+
+// Define initial state
+const initialState = {
+  favs: [],
+  selectedPhoto: null,
+  modal: false,
+};
+
+// Define action types
+const TOGGLE_FAV = 'TOGGLE_FAV';
+const SET_SELECTED_PHOTO = 'SET_SELECTED_PHOTO';
+const CLOSE_PHOTO_DETAILS_MODAL = 'CLOSE_PHOTO_DETAILS_MODAL';
+
+// Create a reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+    case TOGGLE_FAV:
+      return {
+        ...state,
+        favs: state.favs.includes(action.payload)
+          ? state.favs.filter((id) => id !== action.payload)
+          : [...state.favs, action.payload],
+      };
+    case SET_SELECTED_PHOTO:
+      return { ...state, selectedPhoto: action.payload, modal: true };
+    case CLOSE_PHOTO_DETAILS_MODAL:
+      return { ...state, modal: false, selectedPhoto: null };
+    default:
+      return state;
+  }
+};
+
 export const useApplicationData = () => {
-  const [favs, setFavs] = useState([]);
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
-  const [modal, setModal] = useState(false);
+  // Use the reducer to manage state
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Define action functions to update state
   const updateToFavPhotoIds = (photoId) => {
-    favs.includes(photoId) ? setFavs(favs.filter(e => e !== photoId)) : setFavs([...favs, photoId]);
+    dispatch({ type: TOGGLE_FAV, payload: photoId });
   };
-    const setPhotoSelected = (photo) => {
-    setModal(true);
-    setSelectedPhoto(photo);
+
+  const setPhotoSelected = (photo) => {
+    dispatch({ type: SET_SELECTED_PHOTO, payload: photo });
   };
-const onClosePhotoDetailsModal = () => {
-    setModal(false);
-    setSelectedPhoto(null);
+
+  const onClosePhotoDetailsModal = () => {
+    dispatch({ type: CLOSE_PHOTO_DETAILS_MODAL });
   };
+
   return {
-    state: { favs, selectedPhoto, modal },
+    state: state,
     updateToFavPhotoIds,
     setPhotoSelected,
-    onClosePhotoDetailsModal
-
+    onClosePhotoDetailsModal,
   };
 };
